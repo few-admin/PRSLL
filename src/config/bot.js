0da1492ea5a7b1f362deb8d080bef876417ace14
few-admin <uopmilky123@gmail.com>
2026-07-20 10:07:22 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "PRLSS Community",
+        name: "PRGS Community",
         // Activity type number (0 = Playing).
         type: 0,
       },
